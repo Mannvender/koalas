ed41410bf93c880af3ffa69a1bbb83e16acbeec9
mannvender <mannvender001@gmail.com>
2021-08-26 11:30:46 +0530

    updated twitter link

--- components/SecondarySection.js
+++ components/SecondarySection.js
@@ -37,7 +37,7 @@ const SecondarySection = ({
         }}
       >
         <Link
-          href="https://twitter.com"
+          href="https://twitter.com/KaptainKoala99"
           target="_blank"
           rel="noopener noreferrer"
           sx={{
