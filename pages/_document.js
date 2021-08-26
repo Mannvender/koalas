fd446c97374b1beadca79694becb4069a3045d28
mannvender <mannvender001@gmail.com>
2021-08-26 17:45:23 +0530

    updated font-import

--- pages/_document.js
+++ pages/_document.js
@@ -49,11 +49,7 @@ export default class MyDocument extends Document {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700;900&family=Poppins:wght@300;400;500;600&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@900&family=Poppins:wght@400;500;600&display=swap"
           rel="stylesheet"
         />
         <link
